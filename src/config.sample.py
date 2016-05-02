fc7dda816c8ae6e7a062d6ea5304ad34f8ed3a74
Martin Wetterwald <martin@wetterwald.eu>
2016-05-02 22:30:43 +0200

    Creates config module.

---
+++ src/config.sample.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+pushover_app_token = ''
+
+sia_base = 'https://www.sia.aviation-civile.gouv.fr/aip/enligne/Atlas-VAC/FR/'
+sia_link_page = 'VACProduitPartie.htm'
+sia_link_needle = 'Liste de contr'
