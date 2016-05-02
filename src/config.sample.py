566653cbf27dcc59f812ef064a1dcc3ec1de24c3
Martin Wetterwald <martin@wetterwald.eu>
2016-05-03 00:09:37 +0200

    Implements fetching and parsing of VAC Control List PDF.

--- src/config.sample.py
+++ src/config.sample.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-pushover_app_token = ''
-
+pushover_app_token=''
 sia_base = 'https://www.sia.aviation-civile.gouv.fr/aip/enligne/Atlas-VAC/FR/'
 sia_link_page = 'VACProduitPartie.htm'
 sia_link_needle = 'Liste de contr'
+
+pdf_dst = 'ctrl.pdf'
+pdf_regex = "^(?P<ref>(?:AD 2 |AD-2|CTL-[0-9]+|GEN VAC [A-Za-z0-9]+|SIV [A-Z]{3} [0-9]+).*) (?P<date>[0-9]{2} (?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) [0-9]{4})$"
+pdf_min_pages = 15
+pdf_min_records = 1200
--- src/sia.py
+++ src/sia.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import config, urllib2
+import config, urllib2, subprocess, re
 from bs4 import BeautifulSoup
+from pyPdf import PdfFileReader
+from pyPdf.utils import PdfReadError
 
 def _sia_get_ctrl_list_url():
-    ret = urllib2.urlopen(config.sia_base + config.sia_link_page)
-    html = ret.read()
-    soup = BeautifulSoup(html, "lxml")
+    try:
+        ret = urllib2.urlopen(config.sia_base + config.sia_link_page)
+    except(urllib2.URLError):
+        return False
+
+    soup = BeautifulSoup(ret.read(), "lxml")
     tags = soup.find_all('a')
     for tag in tags:
         if unicode(tag.string).startswith(config.sia_link_needle):
             return tag.get('href', None)
+
     return None
+
+def _sia_dl_ctrl_list():
+    url = _sia_get_ctrl_list_url()
+    if not url:
+        return False
+
+    url = config.sia_base + url
+
+    try:
+        ret = urllib2.urlopen(url)
+    except(urllib2.URLError):
+        return False
+
+    f = open(config.pdf_dst, 'wb')
+    f.write(ret.read())
+    f.close()
+
+    try:
+        pdf = PdfFileReader(file(config.pdf_dst))
+    except(IOError, PdfReadError):
+        return False
+
+    if pdf.getNumPages() < config.pdf_min_pages:
+        return False
+
+    return True
+
+def sia_parse_ctrl_list():
+    if not _sia_dl_ctrl_list():
+        return False
+
+    p = subprocess.Popen(["ps2ascii", config.pdf_dst], stdout=subprocess.PIPE)
+    txt = p.communicate()[0]
+    if p.returncode != 0:
+        return False
+
+    r = re.compile(config.pdf_regex, flags=re.MULTILINE)
+
+    dic = [m.groupdict() for m in r.finditer(txt, re.MULTILINE)]
+    if len(dic) < config.pdf_min_records:
+        return False
+
+    return dic
